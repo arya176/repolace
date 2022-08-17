1a2a9fe24d6196441ecef18e29f766e62c7c8bf9
arya <aryaradfar@gmail.com>
2022-08-18 00:55:39 +1000

    final edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,23 +7,24 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar />
+      <Avatar imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      />
 
       <Card
         name={Contacts[0].name}
-        img={Contacts[0].imgURl}
+        img={Contacts[0].imgURL}
         phone={Contacts[0].phone}
         email={Contacts[0].email}
       />
       <Card
         name={Contacts[1].name}
-        img={Contacts[1].imgURl}
+        img={Contacts[1].imgURL}
         phone={Contacts[1].phone}
         email={Contacts[1].email}
       />
       <Card
         name={Contacts[2].name}
-        img={Contacts[2].imgURl}
+        img={Contacts[2].imgURL}
         phone={Contacts[2].phone}
         email={Contacts[2].email}
       />
