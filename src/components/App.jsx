dbe14f5ebe03fdd9b6b7b5c796e90aeae3464de4
arya <aryaradfar@gmail.com>
2022-08-19 01:46:18 +1000

    starting work on page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,16 @@
 import React from "react";
-import Card from "./card";
-import Avatar from "./avatar";
-import Contacts from "../contacts";
 
-function App() {
-  return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <Avatar imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      />
+function App (){
+    return <div>
+    <nav>
+        <ul class="nav">
+            <li><a class="active">ABOUT ME</a></li>
+            <li><a href="table.html" target="_self">EXPERIENCES</a></li>
+            <li><a href="table2.html" target="_self">MY WORKS</a></li>
+        
 
-      <Card
-        name={Contacts[0].name}
-        img={Contacts[0].imgURL}
-        phone={Contacts[0].phone}
-        email={Contacts[0].email}
-      />
-      <Card
-        name={Contacts[1].name}
-        img={Contacts[1].imgURL}
-        phone={Contacts[1].phone}
-        email={Contacts[1].email}
-      />
-      <Card
-        name={Contacts[2].name}
-        img={Contacts[2].imgURL}
-        phone={Contacts[2].phone}
-        email={Contacts[2].email}
-      />
-    </div>
-  );
+        </ul>
+    </nav>
+</div>
 }
-
 export default App;
