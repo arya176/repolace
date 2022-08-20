54062bb2e77db46be173bc221d618c6009e364d0
arya <aryaradfar@gmail.com>
2022-08-20 16:25:10 +1000

    fixed the rendering issue

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -14,9 +14,7 @@ function Aboutme (){
 
             <h2>Who i am</h2>
             <p class="p-main ">I was someone else. actually my job was Civil-Eng. I gratuated in Interior Design long time ago too. Since few years ago i noticed i can bring my interested to Design and mking website. i like design things too much. hope you like what i've done. You can see what i've done before in <q>MY WORKS</q>,and what Experiences i had already in my<q> Experiences</q> tab. i love to know more <b>creative people</b> in the world. be in touch if you like.</p>
-            
 
-    
 
         </center>
 
